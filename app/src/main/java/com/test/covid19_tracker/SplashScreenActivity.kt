1eb75e38f97e2c094f57221747c248cc8d279c5f
Mukul Kumar <mukul.2121@gmail.com>
2020-04-15 22:11:57 +0530

    Added splash screen and icon

---
+++ app/src/main/java/com/test/covid19_tracker/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.test.covid19_tracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long=3000 // 3 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
